481fc50e78d69ff10ff5543349f5a8f8d3e3095e
LightQuantum <self@lightquantum.me>
2022-09-30 16:33:57 +0800

    chore: custom adjustments

--- flake.nix
+++ flake.nix
@@ -1,20 +1,10 @@
 {
   description = "My personal NUR repository";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
-    let
-      systems = [
-        "x86_64-linux"
-        "i686-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "armv6l-linux"
-        "armv7l-linux"
-      ];
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-    in
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils }:
     {
-      packages = forAllSystems (system: import ./default.nix {
+      packages = flake-utils.lib.eachDefaultSystem (system: import ./default.nix {
         pkgs = import nixpkgs { inherit system; };
       });
     };
